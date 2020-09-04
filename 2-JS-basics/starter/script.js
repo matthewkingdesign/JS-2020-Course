d3fd2e5dc0ce6c5b9639d02cc79ad955df2057c1
matthewkingdesign <hello@matthewking.design>
2020-09-04 09:49:41 +0100

    added some more vars and made logged muliple vars using comments

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,13 @@
+// lesson 6
 // set a variable using var
 var firstVar = 'hello world';
 //logged variable to the console
 console.log(firstVar);
+
+// lesson 8
+var firstName = 'John';
+var lastName = 'Smith';
+var age = 28;
+
+// add more than one var with commas between them
+console.log(firstName, lastName, age);
