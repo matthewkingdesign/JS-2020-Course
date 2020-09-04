eca525423eb60bc15b77808fb4868d00a14a2452
matthewkingdesign <hello@matthewking.design>
2020-09-04 10:24:43 +0100

    completed lessons 1 - 8 and coverd basics, vars, primitive data types

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -5,9 +5,19 @@ var firstVar = 'hello world';
 console.log(firstVar);
 
 // lesson 8
+// string primitive data type
 var firstName = 'John';
 var lastName = 'Smith';
+// number primitive data type
 var age = 28;
-
+// boolean primitive data type
+var fullAge = true;
+var underAge = false;
+// undefined primitive data type
+var job;
+// define above job var
+job = 'Teacher';
+// the only way you can have a nu,ber as a variable name is with a $ or a _ before it. no other symbol will work. You cannot use JS keywords either
+var $420 = 420;
 // add more than one var with commas between them
-console.log(firstName, lastName, age);
+console.log(firstName, lastName, age, job);
