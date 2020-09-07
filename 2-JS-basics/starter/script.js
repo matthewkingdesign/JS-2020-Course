588a844d1604ed4b357d2c20e0a5e0eb4cb478dc
matthewkingdesign <hello@matthewking.design>
2020-09-07 18:18:52 +0100

    finished functions and did the challenge. I got it right, but in a different way

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -330,43 +330,43 @@ lesson 18 - Challenge 2
 Basketball 
 
 */
-var johnGame1, johnGame2, johnGame3, johnAverageScore, mikeGame1, mikeGame2, mikeGame3, mikeAverageScore, maryGame1, maryGame2, maryGame3, maryAverageScore;
-// COULD HAVE DONE IT LIKE THIS
-// var johnScore = (89 + 120 + 23) / 3
-johnGame1 = 89;
-johnGame2 = 120;
-johnGame3 = 23;
+// var johnGame1, johnGame2, johnGame3, johnAverageScore, mikeGame1, mikeGame2, mikeGame3, mikeAverageScore, maryGame1, maryGame2, maryGame3, maryAverageScore;
+// // COULD HAVE DONE IT LIKE THIS
+// // var johnScore = (89 + 120 + 23) / 3
+// johnGame1 = 89;
+// johnGame2 = 120;
+// johnGame3 = 23;
 
-// COULD HAVE DONE IT LIKE THIS
-// var mikeScore = (116 + 94 + 123) / 3
-mikeGame1 = 116;
-mikeGame2 = 94;
-mikeGame3 = 123;
+// // COULD HAVE DONE IT LIKE THIS
+// // var mikeScore = (116 + 94 + 123) / 3
+// mikeGame1 = 116;
+// mikeGame2 = 94;
+// mikeGame3 = 123;
 
-// COULD HAVE DONE IT LIKE THIS
-// var maryScore = (97 + 134 + 105) / 3
-maryGame1 = 97;
-maryGame2 = 134;
-maryGame3 = 105;
+// // COULD HAVE DONE IT LIKE THIS
+// // var maryScore = (97 + 134 + 105) / 3
+// maryGame1 = 97;
+// maryGame2 = 134;
+// maryGame3 = 105;
 
-// johnAverageScore = (johnGame1 + johnGame2 + johnGame3) / 3;
-// mikeAverageScore = (mikeGame1 + mikeGame2 + mikeGame3) / 3;
-// maryAverageScore = (maryGame1 + maryGame2 + maryGame3) / 3;
-johnAverageScore = 9;
-mikeAverageScore = 9;
-maryAverageScore = 8;
+// // johnAverageScore = (johnGame1 + johnGame2 + johnGame3) / 3;
+// // mikeAverageScore = (mikeGame1 + mikeGame2 + mikeGame3) / 3;
+// // maryAverageScore = (maryGame1 + maryGame2 + maryGame3) / 3;
+// johnAverageScore = 9;
+// mikeAverageScore = 9;
+// maryAverageScore = 8;
 
-if (johnAverageScore > mikeAverageScore && johnAverageScore > maryAverageScore){
-    console.log('John wins with an average score of ' + johnAverageScore);
-} else if (maryAverageScore > mikeAverageScore && maryAverageScore > johnAverageScore){
-    console.log('Mary wins with an average score of ' + maryAverageScore);
-} else if (mikeAverageScore > maryAverageScore && mikeAverageScore > johnAverageScore){
-    console.log('Mike wins with an average score of ' + mikeAverageScore);
-} else if (johnAverageScore === mikeAverageScore && johnAverageScore === maryAverageScore) {
-    console.log('it was a draw mike\'s average score was ' + mikeAverageScore + ' and john\'s average score was ' + johnAverageScore + ' and mary\'s average score was ' + maryAverageScore);
-} else {
-    console.log('something else happend');
-}
+// if (johnAverageScore > mikeAverageScore && johnAverageScore > maryAverageScore){
+//     console.log('John wins with an average score of ' + johnAverageScore);
+// } else if (maryAverageScore > mikeAverageScore && maryAverageScore > johnAverageScore){
+//     console.log('Mary wins with an average score of ' + maryAverageScore);
+// } else if (mikeAverageScore > maryAverageScore && mikeAverageScore > johnAverageScore){
+//     console.log('Mike wins with an average score of ' + mikeAverageScore);
+// } else if (johnAverageScore === mikeAverageScore && johnAverageScore === maryAverageScore) {
+//     console.log('it was a draw mike\'s average score was ' + mikeAverageScore + ' and john\'s average score was ' + johnAverageScore + ' and mary\'s average score was ' + maryAverageScore);
+// } else {
+//     console.log('something else happend');
+//}
 
 /***********
 lesson 19 - Challenge 2 - Solution
@@ -402,3 +402,209 @@ Basketball
 lesson 20 - Functions
 
 */
+// simple function calculating age
+// function calculateAge(birthyear){
+//     // returns the age
+//     return 2020 - birthyear;
+// }
+// // stores returned value based on parameter after calling the function. Or when you call the function you pass data to it
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1948);
+// var ageMary = calculateAge(1970);
+// console.log(ageJohn, ageMike, ageMary);
+
+// // pass multiple arguments to the function
+// function yearsUntilRetirement (yearBorn, firstName) {
+//     // call previous function to get age based off of argument based
+//     var age = calculateAge(yearBorn);
+//     // calculate retirement years
+//     var retirement = 65 - age; 
+//     // figure out if they have already retired
+//     if (retirement > 0) {
+//         console.log(firstName + ' retires in ' + retirement + ' years.');
+//     } else {
+//         console.log(firstName + ' is already retired');
+//     }
+// }
+// // call function with year born, which is used to figure out age then figure out retirement value and first name.
+// yearsUntilRetirement(1990, 'John');
+// yearsUntilRetirement(1948, 'Mike');
+// yearsUntilRetirement(1969, 'Mary');
+
+/***********
+lesson 21 - Function Statements and Expressions
+
+*/
+
+// // function decleration
+// // function whatDoYouDo(job ,firstName){}
+
+// // function expression 
+// // using a switch statement, no need for 'break' as we are using return, which stops the function and rturns the value to the function call. 
+// var whatDoYouDo = function(job, firstName){
+//     switch(job){
+//         case 'teacher':
+//             return firstName + ' teaches kids how to code';
+//         case 'driver':
+//             return firstName + ' drives an Uber';
+//         case 'designer':
+//             return firstName + ' makes nice websites';
+//         default:
+//             return firstName + ' does something else';
+//     }
+// }
+// // log result striaght to conolse
+// console.log(whatDoYouDo('teacher', 'John'));
+// console.log(whatDoYouDo('driver', 'Dave'));
+// console.log(whatDoYouDo('designer', 'Bob'));
+// console.log(whatDoYouDo('builder', 'Bill'));
+// // save result in var then log to console
+// var youDo = whatDoYouDo('teacher', 'John');
+// console.log(youDo);
+
+// difference between expressions and statements. Exressions produce results, give us values, statements are actions. 
+
+/***********
+lesson 22 - Arrays
+
+*/
+
+// // Here are two different ways of creating arrays
+// // arrays are 0 based so the first value is 0 in teh list. 0, 1, 2, 3... 2 is the 3rd value. 0 is the 1st.
+// var names = ['John', 'Mark', 'Jane'];
+// var years = new Array(1990, 1969, 1948);
+// //display whole array
+// console.log(names);
+// // access first value
+// console.log(names[0]);
+// //display how many values there are
+// console.log(names.length);
+
+// //mutate the array
+// names[1] = 'Ben';
+// // Add a value in a specific place
+// // names[5] = 'Mary';
+// // Add a value at the end
+// names[names.length] = 'Dave';
+// console.log(names);
+
+// // Different data types
+// var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// //use the 'push' method (a function specific to arrays) to add a value at the end of the array
+// john.push('blue');
+// john.unshift('Mr.');
+// console.log(john);
+// // pop method removes value from the end of the array, below we are removing two
+// john.pop();
+// john.pop();
+// //shift removes from the start of the array
+// john.shift();
+// console.log(john);
+
+// // indexOf method show us the position of the value passed in the argument
+// console.log(john.indexOf(1990));
+
+// //Test if value is in the array. If it is not it will evaluate as -1
+// var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer';
+// console.log(isDesigner);
+
+/***********
+lesson 23 - Coding Challenge 3
+
+*/
+
+// var meal1, meal2, meal3;
+
+// meal1 = 124;
+// meal2 = 48;
+// meal3 = 268;
+
+// function calculateTip(mealCost){
+//     if (mealCost <= 50){
+//         return mealCost * 0.2;
+//     } else if (mealCost > 50 && mealCost <= 200){
+//         return mealCost * 0.15;
+//     } else {
+//         return mealCost * 0.1;
+//     }
+// }
+
+// var meal1tip = calculateTip(124);
+// var meal2tip = calculateTip(48);
+// var meal3tip = calculateTip(268);
+
+// var tips = [meal1tip, meal2tip, meal3tip];
+// var totalCost = [(meal1 + meal1tip), (meal2 + meal2tip), (meal3 + meal3tip)]
+
+// console.log(tips);
+// console.log(totalCost);
+
+/***********
+lesson 24 - Coding Challenge 3 Solution
+
+*/
+
+// COULD HAVE DONE IT LIKE THIS
+
+// function tipCalculator(bill){
+//     var percentage
+//     if (bill < 50) {
+//         percentage = 0.2;
+//     } else if (bill >= 50 && bill < 200) {
+//         percentage = 0.15;
+//     } else {
+//         percentage = 0.1;
+//     }
+//     return percentage * bill;
+// }
+
+// var bills = [124, 48, 268];
+// var tips = [
+//     tipCalculator(bills[0]),
+//     tipCalculator(bills[1]),
+//     tipCalculator(bills[2])
+// ];
+
+// var finalValues = [
+//     bills[0] + tips[0],
+//     bills[1] + tips[1],
+//     bills[2] + tips[2]
+// ];
+
+// console.log(tips, finalValues);
+
+/***********
+lesson 25 = Objects and properties
+
+*/
+// Objects order doesnt matter where as arrays it does.
+// key names are paired with values
+// first name is a property of the john object
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'Teacher',
+    isMarried: false
+};
+// use . syntax to access properties
+console.log(john.firstName);
+// [] syntax to access properties
+console.log(john['lastName']);
+// Save birthyear keyname propety pair in a variable and call it
+var x = 'birthYear';
+console.log(john[x]);
+
+// Mutate the Object
+john.job = 'designer';
+john['isMarried'] = true;
+
+console.log(john);
+
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.lastName = 'Smith';
+jane.birthYear = 1991;
+console.log(jane);
