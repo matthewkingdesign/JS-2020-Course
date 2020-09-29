0bcac2f9d8f8f83cdc535236a4f23cfa63ba49b1
matthewkingdesign <hello@matthewking.design>
2020-09-29 14:58:55 +0100

    completed lecture 65

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -224,6 +224,29 @@ When we try and access a property or methos JS will look for the the method or p
 //  console.log(ages);
 //  console.log(fullAges);
 //  console.log(heartRates);
+/******************************************************
+ Lecture 66 -  First Class Functions: Functions Returning Functions
 
+*/
+function interviewQuestion(job){
+    if(job === 'designer') {
+        return function(name){
+            console.log(name + ', can you please explain what UX is');
+        }
+    } else if (job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('John');
 
+interviewQuestion('teacher')('Mark');
