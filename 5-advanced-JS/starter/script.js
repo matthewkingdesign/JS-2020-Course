48b8157f792e9037963d34b7579954261e1304bc
matthewkingdesign <hello@matthewking.design>
2020-10-08 09:47:36 +0100

    starting coding challenge 7

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -316,3 +316,99 @@ When we try and access a property or methos JS will look for the the method or p
  Lecture 69 - Bind, Call and Apply
  
 */
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher',
+//     presentation: function(style, timeOfDay) {
+//         if (style === 'formal') {
+//             console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//         } else if (style === 'friendly') {
+//             console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//         }
+//     }
+// };
+
+// var emily = {
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// };
+
+// john.presentation('formal', 'morning');
+// // we call the function and attach it to emily. emily is now the 'this' variable. So the first argument of the call method is the new this var you want.
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+// // the apply method does a similar thing but with a array as the second input/holder for the rest of the inputs.
+// john.presentation.apply(emily, ['formal', 'morning']);
+// // Bind allows us to store functions with preset arguments
+// // The bind method allows us to store a copy of the function with a set this var a parameter inside a var, then call that var (which is a function) and pass it the second parameter.
+// // This is called carrying. Creating a function based on another function with some preset parameters
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+// johnFriendly('morning');
+// johnFriendly('night');
+
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+// emilyFormal('afternoon');
+
+
+// // Another cool example
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el) {
+//     return 2016 - el;
+// }
+
+// function isFullAge(limit, el) {
+//     return el >= limit;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// // this passes a copy of the isFullAge fucntion with a preset parameter of 20 
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// console.log(ages);
+// console.log(fullJapan);
+
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+
+// --- Let's build a fun quiz game in the console! ---
+
+// 1. Build a function constructor called Question to describe a question. A question should include:
+// a) question itself
+// b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+// c) correct answer (I would use a number for this)
+
+// 2. Create a couple of questions using the constructor
+
+// 3. Store them all inside an array
+
+// 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+// 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+// 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+// 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
+// --- Expert level ---
+
+// 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+// 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+// 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+// 11. Display the score in the console. Use yet another method for this.
+
